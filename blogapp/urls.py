e56614dd292c3bd17c232ccbde87c06334125f3e
Al3xandruF <al3x.florea@gmail.com>
2023-07-16 12:15:22 +0000

    Update urls.py/index.html for errors

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', viewws.index, name="index")
+    path('', views.index, name="index"),
     path('post/<slug:slug>', views.post_page, name='post_page')
 ]
