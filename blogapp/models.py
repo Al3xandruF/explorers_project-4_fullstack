26ac1f58f8da8853174cf8e5a75231ee458a7f55
Al3xandruF <al3x.florea@gmail.com>
2023-07-16 20:00:39 +0000

    Add save method for tags

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -7,6 +8,11 @@ class Tag(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     slug = models.Slugfield(max_length=200, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        return super(Tag, self).save(*args, **kwargs)
 
 
 class Post(models.Model):
