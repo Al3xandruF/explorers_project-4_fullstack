54e55979c53f1c3e6f761c84402b0fcfde72d2d0
Al3xandruF <al3x.florea@gmail.com>
2023-07-16 12:04:16 +0000

    Add index view for homepage

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', viewws.index, name="index")
     path('post/<slug:slug>', views.post_page, name='post_page')
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -5,6 +5,12 @@ from blogapp.models import Post
 # Create your views here.
 
 
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'app/index.html', context)
+
+
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     context = {'post': post}
