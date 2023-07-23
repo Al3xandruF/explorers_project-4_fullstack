60267d9332732259b87b839dbf419c4a8906e0a5
Al3xandruF <al3x.florea@gmail.com>
2023-07-23 14:09:25 +0000

    Update index.html/views.py for subscribe form

--- blogapp/views.py
+++ blogapp/views.py
@@ -13,7 +13,15 @@ def index(request):
     top_posts = Post.objects.all().order_by('-view_count')[0:3]
     recent_posts = Post.objects.all().order_by('last_updated')[0:3]
     subscribe_form = SubscribeForm()
-    context = {'posts': posts, 'top_posts': top_posts, 'recent_posts': recent_posts, 'subscribe_form': subscribe_form}
+    subscribe_successful = None
+
+    if request.POST:
+        subscribe_form = SubscribeForm(request.POST)
+        if subscribe_form.is_valid():
+            subscribe_form.save()
+            subscribe_successful = 'Subscribed Successfully'
+
+    context = {'posts': posts, 'top_posts': top_posts, 'recent_posts': recent_posts, 'subscribe_form': subscribe_form, 'subscribe successful': subscribe_successful}
     return render(request, 'app/index.html', context)
 
 
