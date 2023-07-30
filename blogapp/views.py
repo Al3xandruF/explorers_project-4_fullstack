12aaee1b6fb0cd561e8573bc96179705116e1474
Al3xandruF <al3x.florea@gmail.com>
2023-07-30 14:02:46 +0000

    Update index in views for storing session data

--- blogapp/views.py
+++ blogapp/views.py
@@ -29,6 +29,7 @@ def index(request):
         subscribe_form = SubscribeForm(request.POST)
         if subscribe_form.is_valid():
             subscribe_form.save()
+            request.session['subscribed'] = True
             subscribe_successful = 'Subscribed Successfully'
             subscribe_form = SubscribeForm()
 
