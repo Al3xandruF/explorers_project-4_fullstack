465c7b1d60d06ca76f5b938bc102e7f5dd148080
Al3xandruF <al3x.florea@gmail.com>
2023-07-30 11:43:08 +0000

    Add websitemeta model/import to admin

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from blogapp.models import Comment, Post, Tag, Profile
+from blogapp.models import Comment, Post, Tag, Profile, WebsiteMeta
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Comment)
 admin.site.register(Profile)
+admin.site.register(WebsiteMeta)
