f9834187ccf0ad20c235e80054357f8e05c3c0e4
Al3xandruF <al3x.florea@gmail.com>
2023-09-09 10:08:43 +0000

    Add heroku app name for deployment

--- explorers/settings.py
+++ explorers/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['explorers-4th-project-fs-17cd58235910.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = ['8000-al3xandruf-explorerspro-puc895b1k7q.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['explorers-4th-project-fs-17cd58235910.herokuapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-al3xandruf-explorerspro-puc895b1k7q.ws-eu104.gitpod.io']
 
