918057c67d9074ebe70a70608042f64a5ae7802d
Al3xandruF <al3x.florea@gmail.com>
2023-12-15 18:10:17 +0000

    Update html files

--- explorers/settings.py
+++ explorers/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['explorers-4th-project-fs-17cd58235910.herokuapp.com', 'localhost']  # noqa
-# ALLOWED_HOSTS = ['8000-al3xandruf-explorerspro-puc895b1k7q.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-al3xandruf-explorerspro-puc895b1k7q.ws-eu107.gitpod.io']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-al3xandruf-explorerspro-puc895b1k7q.ws-eu107.gitpod.io']  # noqa
 
