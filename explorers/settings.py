65d507bc7354760ab290e3f0e5d3a07a95221309
Al3xandruF <al3x.florea@gmail.com>
2023-12-15 14:22:48 +0000

    Update settings for heroku deployment

--- explorers/settings.py
+++ explorers/settings.py
@@ -39,10 +39,10 @@ SESSION_COOKIE_PATH = '/'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['explorers-4th-project-fs-17cd58235910.herokuapp.com', 'localhost']  # noqa
-ALLOWED_HOSTS = ['8000-al3xandruf-explorerspro-puc895b1k7q.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['explorers-4th-project-fs-17cd58235910.herokuapp.com', 'localhost']  # noqa
+# ALLOWED_HOSTS = ['8000-al3xandruf-explorerspro-puc895b1k7q.ws-eu107.gitpod.io']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-al3xandruf-explorerspro-puc895b1k7q.ws-eu107.gitpod.io']  # noqa
 
